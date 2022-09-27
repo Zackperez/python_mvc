66807f87e547f7aa64088aea835eb4aaf8015b0c
Zacks <53922473+Zackperez@users.noreply.github.com>
2022-09-26 22:39:39 -0500

    Ahora se guarda el resultado dentro de un JSON

--- nosebro.py
+++ nosebro.py
@@ -1,14 +1,22 @@
 import tkinter as tk
+import json
 
 class Model:
     def __init__(self):
         self.n1 = tk.IntVar(0)
+        self.n2 = tk.IntVar(0)
 
-    def get_numero(self):
+    def get_numero1(self):
         return self.n1
 
-    def set_numero(self,n1):
+    def set_numero1(self,n1):
         self.n1 = n1
+
+    def get_numero2(self):
+        return self.n2
+
+    def set_numero2(self,n2):
+        self.n2 = n2
 
 class View (tk.Frame):
     def __init__(self, parent):
@@ -20,6 +28,11 @@ class View (tk.Frame):
         self.txtn1=tk.Entry()
         self.txtn1.grid(row=0,column=1)
 
+        self.lbln2=tk.Label(self.parent, text="Numero 2")
+        self.lbln2.grid( row=0,column=2)
+        self.txtn2=tk.Entry()
+        self.txtn2.grid(row=0,column=3)
+
         self.btnguardar=tk.Button(text="Guardar")
         self.btnguardar.grid(row=2,column=0)
 
@@ -30,9 +43,10 @@ class View (tk.Frame):
         self.lblres.grid(row=3,column=0)
 
         self.parent.geometry("500x300")
+        self.parent.resizable(0, 0)
 
     def mostrar_resultado(self, message):
-        self.lblres['text'] = message        
+        self.lblres['text'] = message
 
 class Controller:
     def __init__(self, root):
@@ -40,17 +54,62 @@ class Controller:
         self.view = View(root)
 
         self.view.btnguardar.config(command=self.guardar_numero)
-        self.view.btnmostrar.config(command=self.muestra_numero)
+        self.view.btnmostrar.config(command=self.suma)
 
     def guardar_numero(self):
-        self.model.set_numero(self.view.txtn1.get())    
+        try:
+            self.model.set_numero1(int(self.view.txtn1.get()))
+            self.model.set_numero2(int(self.view.txtn2.get()))
+        except:
+            self.borrar_campos()
+            self.view.lblres['text'] = "Verifica que sean números"
+
+    def borrar_campos(self):
+        self.view.txtn1.delete(0, tk.END)
+        self.view.txtn2.delete(0, tk.END)
 
     def muestra_numero(self):
+        n1 = self.model.get_numero1()
+        self.view.lblres['text'] = n1
+
+    def suma(self):
         try:
-            n1 = self.model.get_numero()
-            self.view.lblres['text'] = n1
+            n1 = self.model.get_numero1()
+            n2 = self.model.get_numero2()
+            suma = n1 + n2
+            print(suma)
+            self.view.lblres['text'] = "La suma es",suma
+            texto_res = self.view.lblres.cget("text")
+            self.agregar_datos_generales_json(n1,n2,texto_res)
         except Exception as e:
-            print(e)
+            print("Ocurrió un error xd", e)
+
+    def agregar_datos_generales_json(self, n1,n2,res):
+        informacion_json_final = []
+
+        if self.existe_historial() == True:
+            nuevos_datos = {"numero1": n1, "numero2": n2, "resultado": res}
+            with open("historial.json") as archivo_json:
+                datos = json.load(archivo_json)
+            datos.append(nuevos_datos)
+
+            with open("historial.json", 'w') as archivo_json:
+                json.dump(datos, archivo_json, indent=3)
+                print("Se han añadido los siguientes datos al archivo " + archivo_json.name+"\n")
+        else:    
+            
+            informacion_usuario = {"numero1": n1, "numero2": n2, "resultado": res}
+            with open("historial.json", 'w') as archivo_json:
+                informacion_json_final.append(informacion_usuario)
+                json.dump(informacion_json_final, archivo_json, indent=3)
+                print(archivo_json.name+" creado exitosamente")                      
+
+    def existe_historial(self):
+        try:
+            with open('historial.json') as archivo:
+                return True
+        except FileNotFoundError as e:
+            return False        
 
 if __name__ == "__main__":
     root = tk.Tk()
